b72dffdb57bac8b77811d0f4f8256e9761bf1675
Ivan Parkhomchuk <workaccbyivan@gmail.com>
2025-11-17 23:12:23 +0200

    PaymentIntent fix (Stripe)

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -5,18 +5,24 @@ import { CreateChargeDto } from '@app/common';
 
 @Injectable()
 export class PaymentsService {
-  private readonly stripe = new Stripe(
-    this.configService.get('STRIPE_SECRET_KEY'),
-  );
+  private stripe: Stripe;
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {
+    this.stripe = new Stripe(
+      this.configService.get<string>('STRIPE_SECRET_KEY'),
+    );
+  }
 
   async createCharge({ amount }: CreateChargeDto) {
-    const paymentIntent = await this.stripe.paymentIntents({
+    const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
       payment_method: 'pm_card_visa',
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never',
+      },
     });
 
     return paymentIntent;
